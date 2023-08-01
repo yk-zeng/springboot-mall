637a665772a73dbdf2ff4df0e1e765245f520c9a
yk.zeng <yk.zeng@agile-t.com>
2023-08-01 11:05:53 +0900

    商品功能 - Refactor Category，使用Enum類型

--- springboot-mall/src/main/java/com/tseng/Mapper/ProductRowMapper.java
+++ springboot-mall/src/main/java/com/tseng/Mapper/ProductRowMapper.java
@@ -1,5 +1,6 @@
 package com.tseng.Mapper;
 
+import com.tseng.constant.ProductCategory;
 import com.tseng.model.Product;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -14,11 +15,9 @@ public class ProductRowMapper implements RowMapper<Product> {
         Product product = new Product();
         product.setProductId(rs.getInt("product_id"));
         product.setProductName(rs.getString("product_name"));
-        product.setCategory(rs.getString("category"));
+//        product.setCategory(rs.getString("category"));
 
-//        String categoryStr = rs.getString("category");
-//        ProductCategory productCategory = ProductCategory.valueOf(categoryStr);
-//        product.setCategory(productCategory);
+        product.setCategory(ProductCategory.valueOf(rs.getString("category")));
 
         product.setImageUrl(rs.getString("image_url"));
         product.setPrice(rs.getInt("price"));
--- springboot-mall/src/main/java/com/tseng/model/Product.java
+++ springboot-mall/src/main/java/com/tseng/model/Product.java
@@ -1,13 +1,15 @@
 package com.tseng.model;
 
 
+import com.tseng.constant.ProductCategory;
+
 import java.util.Date;
 
 public class Product {
 
     private Integer productId;
     private String productName;
-    private String category;
+    private ProductCategory category;
     private String imageUrl;
     private Integer price;
     private Integer stock;
@@ -31,11 +33,11 @@ public class Product {
         this.productName = productName;
     }
 
-    public String getCategory() {
+    public ProductCategory getCategory() {
         return category;
     }
 
-    public void setCategory(String category) {
+    public void setCategory(ProductCategory category) {
         this.category = category;
     }
 
